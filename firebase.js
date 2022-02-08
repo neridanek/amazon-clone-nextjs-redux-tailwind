982ee3d8a83f7530d2924c99648cdf03ee14952a
Neridanek <wiktorn.op@gmail.com>
2022-02-08 19:50:17 +0100

    ..

--- firebase.js
+++ firebase.js
@@ -1,8 +1,12 @@
+import { initializeApp } from "firebase/app"
+import firebase from "firebase"
 const firebaseConfig = {
-    apiKey: "AIzaSyCrSy6pceL6cM_6XtZsaT9QqY9VUEKrvTo",
-    authDomain: "amazo-a93f6.firebaseapp.com",
-    projectId: "amazo-a93f6",
-    storageBucket: "amazo-a93f6.appspot.com",
-    messagingSenderId: "636122606900",
-    appId: "1:636122606900:web:eacc7cf1501b5d67168961"
-  };
+  apiKey: "AIzaSyDeyyzlkfqqr5QV9NDoIIl2SiC_IKUBFxI",
+  authDomain: "main-reducer-316118.firebaseapp.com",
+  projectId: "main-reducer-316118",
+  storageBucket: "main-reducer-316118.appspot.com",
+  messagingSenderId: "873089566521",
+  appId: "1:873089566521:web:372c5d9f6ac7eb788ecae8"
+};
+
+const app = initializeApp(firebaseConfig)
